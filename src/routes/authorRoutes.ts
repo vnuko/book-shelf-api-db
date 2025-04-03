30aa4e6b8d7e29cb8dc6b8793d8351339bf2c2c1
Milos Vnucko <milosz.milosz@gmail.com>
2025-04-03 14:39:12 +0200

    small fix in author routing order

--- src/routes/authorRoutes.ts
+++ src/routes/authorRoutes.ts
@@ -4,7 +4,7 @@ import authorController from "./../controllers/AuthorController";
 const router: Router = Router();
 
 router.get("/", authorController.getAuthors);
-router.get("/:authorId", authorController.getAuthor);
 router.get("/search", authorController.searchAuthors);
+router.get("/:authorId", authorController.getAuthor);
 
 export default router;
